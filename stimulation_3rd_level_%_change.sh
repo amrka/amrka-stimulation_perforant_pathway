bd36b212cd8515afc051469504c08156f8231886
Amr <amr.kamel@uv.es>
2020-10-07 17:49:47 +0200

    correct the change_files scripts directory

--- stimulation_3rd_level_%_change.sh
+++ stimulation_3rd_level_%_change.sh
@@ -35,13 +35,13 @@ done
 # change file names to contain gp name
 
 
-python3 /Users/amr/Dropbox/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
+python3 /home/in/aeed/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
 /home/in/aeed/Work/stimulation/stimulation_3rd_level/10Hz/timeseries -7 -4
 
 
-python3 /Users/amr/Dropbox/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
+python3 /home/in/aeed/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
 /home/in/aeed/Work/stimulation/stimulation_3rd_level/20Hz/timeseries -7 -4
 
 
-python3 /Users/amr/Dropbox/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
+python3 /home/in/aeed/SCRIPTS/change_files_to_contain_gp_name_perforant.py \
 /home/in/aeed/Work/stimulation/stimulation_3rd_level/40Hz/timeseries -7 -4
