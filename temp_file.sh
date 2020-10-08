231c787d752e2077b69f3fc54bc3ca509c63b40e
Amr <amr.kamel@uv.es>
2020-10-08 17:28:53 +0200

    tmp file to run create_voxelwise_stat_figs.sh

---
+++ temp_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# temp file to run create_voxelwise_stat_figs.sh
+
+
+./create_voxelwise_stat_figs.sh  10Hz
+./create_voxelwise_stat_figs.sh  20Hz
+./create_voxelwise_stat_figs.sh  40Hz
